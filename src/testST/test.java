85e49b0f9690423ebb4abf2d3313dfadb72b829b
hanyatsu <hanyatsu@gmail.com>
2017-12-09 10:47:09 +0800

    new 算是

--- src/testST/test.java
+++ src/testST/test.java
@@ -4,5 +4,9 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("七八九");
+		int a = 2;
+		int b = 1;
+		int c = a+b;
+		System.out.println(c);
 	}
 }
